21593015b9e019a722247e2de1812bac147a1acb
Toby Hede <tobyhede@info-architects.net>
2012-01-29 20:54:50 +1100

    set version number

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject lein-git-deps "1.0.0-SNAPSHOT"
+(defproject lein-git-deps "0.0.1-SNAPSHOT"
   :description "Attach git dependencies to a leiningen project"
   :dependencies [[org.clojure/clojure "1.3.0"]])
--- test/lein_git_deps/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns lein-git-deps.test.core
-  (:use [lein-git-deps.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
