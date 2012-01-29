eeb16210cf1908fe11b16ca71ae7324ed73347b8
Toby Hede <tobyhede@info-architects.net>
2012-01-29 21:13:57 +1100

    rename src with underscore

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject lein-git-deps "0.0.1-SNAPSHOT"
   :description "Attach git dependencies to a leiningen project"
+  :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.3.0"]])
