d6e21a96cb7d58dd751cfeaf35066cb439baba36
Toby Hede <tobyhede@info-architects.net>
2012-01-29 20:53:08 +1100

    initial extraction and import

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject lein-git-deps "1.0.0-SNAPSHOT"
+  :description "Attach git dependencies to a leiningen project"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/lein_git_deps/test/core.clj
@@ -0,0 +1,6 @@
+(ns lein-git-deps.test.core
+  (:use [lein-git-deps.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
