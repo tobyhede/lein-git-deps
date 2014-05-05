da5d36ebe6058720b1efc3f5d55c4cca4fc47831
Toby Hede <tobyhede@info-architects.net>
2014-05-06 09:09:42 +1000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-git-deps "0.0.1-SNAPSHOT"
+(defproject lein-git-deps "0.0.2-SNAPSHOT"
   :description "Attach git dependencies to a leiningen project"
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.3.0"]])
